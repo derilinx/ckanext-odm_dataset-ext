593055cdc3faf87d30d31c12178381d43e570293
Eric Soroos <eric@derilinx.com>
2018-10-22 14:03:18 +0100

    owner org added in, copied to CI_ResponsibleParty for display.
    Note: MUST change owner_org, as CI_ResponsibleParty is read only
    AAlsso Note: Removing the direct schema changes, as they're
    overwritten by the json.

--- ckanext/odm_dataset_ext/plugin.py
+++ ckanext/odm_dataset_ext/plugin.py
@@ -73,142 +73,20 @@ class Odm_Dataset_ExtPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm)
             log.info('before_create')
 
     def after_create(self, context, pkg_dict):
-
         dataset_type = context['package'].type if 'package' in context else pkg_dict['type']
         if dataset_type == 'dataset':
             log.info('after_create: %s', pkg_dict['name'])
 
 
     def after_update(self, context, pkg_dict):
-
         dataset_type = context['package'].type if 'package' in context else pkg_dict['type']
         if dataset_type == 'dataset':
             log.info('after_update: %s', pkg_dict['name'])
 
-
-
-
-
-    def create_package_schema(self):
-        schema = super(Odm_Dataset_ExtPlugin, self).create_package_schema()
-        #schema = self._modify_package_schema(schema)
-        schema.update({
-            'CI_citation_title': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_abstract': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_topicCategory': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_keywords': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_citation_identAuth': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_language': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_constraints': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_legalConstraints': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_ResponsibleParty': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_format_version': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_Citation_date': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_metadata_dateStamp': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_citation_lastRevision': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_citation_updatefreq': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'Ex_TemporalExtent_startdate': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'Ex_TemporalExtent_enddate': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'Ex_geoname': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_west': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_east': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_south': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_north': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_DataIdentification_spatialResolution': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_spatialreferencesystem': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_PositionalAccuracy': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_QuantitativeAttribute': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_logicalConsistency': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_completeness': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'LI_ProcessStep': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'LI_Lineage': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_ResponsibleParty_contact': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_metadata_contact': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_scopeDescription_attributes': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')]
-
-            })
-
-        return schema
-
-    def update_package_schema(self):
-        schema = super(Odm_Dataset_ExtPlugin, self).update_package_schema()
-        #schema = self._modify_package_schema(schema)
-        schema.update({
-            'CI_citation_title': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_abstract': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_topicCategory': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_keywords': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_citation_identAuth': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_language': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_constraints': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_legalConstraints': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_ResponsibleParty': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_format_version': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_Citation_date': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_metadata_dateStamp': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_citation_lastRevision': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_citation_updatefreq': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'Ex_TemporalExtent_startdate': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'Ex_TemporalExtent_enddate': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'Ex_geoname': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_west': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_east': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_south': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_north': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_DataIdentification_spatialResolution': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_spatialreferencesystem': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_PositionalAccuracy': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_QuantitativeAttribute': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_logicalConsistency': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_completeness': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'LI_ProcessStep': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'LI_Lineage': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_ResponsibleParty_contact': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_metadata_contact': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_scopeDescription_attributes': [toolkit.get_converter('convert_to_extras'), toolkit.get_validator('ignore_missing')]
-        })
-
-        return schema
-
-    def show_package_schema(self):
-        schema = super(Odm_Dataset_ExtPlugin, self).show_package_schema()
-        #schema = self._modify_package_schema(schema)
-        schema.update({
-            'CI_citation_title': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_abstract': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_topicCategory': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_keywords': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_citation_identAuth': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_language': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_constraints': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_legalConstraints': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_ResponsibleParty': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_format_version': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_Citation_date': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_metadata_dateStamp': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_citation_lastRevision': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_citation_updatefreq': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'Ex_TemporalExtent_startdate': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'Ex_TemporalExtent_enddate': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'Ex_geoname': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_west': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_east': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_south': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'EX_GeographicBoundingBox_north': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_DataIdentification_spatialResolution': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_dataidentification_spatialreferencesystem': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_PositionalAccuracy': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_QuantitativeAttribute': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_logicalConsistency': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'DQ_completeness': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'LI_ProcessStep': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'LI_Lineage': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'CI_ResponsibleParty_contact': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_metadata_contact': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'MD_scopeDescription_attributes': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')]
-        })
-        return schema
-
+    def after_show(self, context, pkg_dict):
+        log.debug(pkg_dict)
+        pkg_dict['CI_ResponsibleParty'] = pkg_dict['organization']
+        return pkg_dict
 
     # IConfigurer
     def update_config(self, config_):
