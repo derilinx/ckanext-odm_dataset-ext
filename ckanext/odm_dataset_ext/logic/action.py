11650dfd338a5f2dcabab1543c7634ad632c091e
Eric Soroos <eric@derilinx.net>
2019-04-09 16:42:01 +0000

    for the profiles, allow any odm-spatial-range when I'm specifically searching for them

--- ckanext/odm_dataset_ext/logic/action.py
+++ ckanext/odm_dataset_ext/logic/action.py
@@ -13,12 +13,13 @@ def package_search(context, data_dict):
 
     try:
         query = data_dict.get('fq_list', [])
-        query.extend(['extras_odm_spatial_range:%s' % config.get('ckanext-odm_dataset_ext.country_code')])
-        data_dict['fq_list'] = query
+        if not any('odm_spatial_range' in q for q in query):
+            query.extend(['extras_odm_spatial_range:%s' % config.get('ckanext-odm_dataset_ext.country_code')])
+            data_dict['fq_list'] = query
         return get_actions.package_search(context, data_dict)
 
     except Exception as e:
-        log.error('Something wrong with dataset fileter - ', e)
+        log.error('Something wrong with dataset filter - ', e)
         return get_actions.package_search(context, data_dict)
 
 
