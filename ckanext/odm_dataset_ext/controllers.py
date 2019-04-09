0d7bfb340f764c8f4895f7c1dc29c1a34545875e
Eric Soroos <eric@derilinx.net>
2019-04-09 16:41:29 +0000

    added single feature view of map for detail page

--- ckanext/odm_dataset_ext/controllers.py
+++ ckanext/odm_dataset_ext/controllers.py
@@ -4,7 +4,7 @@ from ckan.plugins.toolkit import c, _
 from ckan.common import request
 from ckan.lib.base import abort
 
-
+import requests
 
 import logging
 log = logging.getLogger(__name__)
@@ -18,6 +18,27 @@ class OdmDataset(PackageController):
             return self.read(package['results'][0]['id'])
         else:
             abort(404, _('Dataset not found'))
+
+
+    def _preflight_wms(self, params, wms_resource):
+        try:
+            params = {"cql_filter": " AND ".join(["%s='%s'" % s for s in params.items()]),
+                      "exceptions":'application/vnd.ogc.se_xml',
+                      'feature_count':'101',
+                      'service':'WMS',
+                      'version':'1.1.1',
+                      'request':'GetMap',
+                      'layers': wms_resource['wms_layer'],
+                      'width':'1',
+                      'height':'1',
+                      'format':'image/png',
+                      'bbox':'0,0,1,1',
+            }
+            resp = requests.get(wms_resource['wms_server'], params=params)
+            return resp.headers['Content-Type'] == 'image/png'
+        except Exception as msg:
+            log.error("Error preflighting cql request: %s" % e)
+            return False
         
     def resource_read_detail(self, id, rid):
         """ url should be dataset/[id]/resource/[rid]/detail?field=value """
@@ -43,6 +64,12 @@ class OdmDataset(PackageController):
             c.fields = results['fields']
             c.resource = resource
             c.pkg_dict = toolkit.get_action('package_show')({}, {'id': id})
+            c.params = params
+            try:
+                c.wms_resource = [r for r in c.pkg_dict['resources'] if r['format'] == 'WMS' and
+                                  self._preflight_wms(params, r)]
+            except:
+                c.wms_resource = None
 
             return toolkit.render('package/resource_detail.html')
         else:
