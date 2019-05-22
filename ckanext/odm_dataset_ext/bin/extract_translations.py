4fb1420a94bf33558b172ff3cf19f0bb7af0d191
Eric Soroos <eric@derilinx.net>
2019-05-22 14:14:46 +0000

    update schema extractor for more items

--- ckanext/odm_dataset_ext/bin/extract_translations.py
+++ ckanext/odm_dataset_ext/bin/extract_translations.py
@@ -2,8 +2,8 @@ import json
 import csv
 
 languages = ["en", "th", "km", "vi", "my"]
-fields = ["field_name", "label"]
-fields.extend(languages)
+fields = ["field_name", "label", 'en', 'Context']
+fields.extend(languages[1:])
 
 
 def extract_one(source, dest):
@@ -20,7 +20,8 @@ def extract_one(source, dest):
         for item in ('label', 'form_placeholder', 'help_text'):
             if not item in field: continue
             elt = { 'field_name': field['field_name'],
-                    'label': item
+                    'label': item,
+                    'Context': "%s.%s" % (field['field_name'], item),
             }
             if type(field[item]) == type({}):
                 for lang in languages:
@@ -43,3 +44,5 @@ extract_one('../odm_dataset_schema.json', 'odm_dataset_schema.csv')
 extract_one('../../../../' + 'ckanext-odm_laws/ckanext/odm_laws/odm_laws_schema.json', 'odm_laws_schema.csv')
 extract_one('../../../../' + 'ckanext-odm_library/ckanext/odm_library/odm_library_schema.json', 'odm_library_schema.csv')
 extract_one('../../../../' + 'ckanext-odm_agreement/ckanext/odm_agreement/odm_agreement_schema.json', 'odm_agreement_schema.csv')
+extract_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_profiles_schema.json', 'odm_profile.csv')
+extract_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_map_schema.json', 'odm_map.csv')
