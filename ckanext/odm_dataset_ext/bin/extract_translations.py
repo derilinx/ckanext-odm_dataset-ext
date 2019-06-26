82688a731b718d0927cd62b3e526d2d15ae06ffd
Eric Soroos <eric@derilinx.com>
2019-06-26 14:11:43 +0000

    Changes to re-import translations.

--- ckanext/odm_dataset_ext/bin/extract_translations.py
+++ ckanext/odm_dataset_ext/bin/extract_translations.py
@@ -1,7 +1,7 @@
 import json
 import csv
 
-languages = ["en", "th", "km", "vi", "my"]
+languages = ["en", "th", "km", "vi", "my", 'lo']
 fields = ["field_name", "label", 'en', 'Context']
 fields.extend(languages[1:])
 
@@ -40,9 +40,50 @@ def extract_one(source, dest):
         for item in translations:
             writer.writerow(item)
 
-extract_one('../odm_dataset_schema.json', 'odm_dataset_schema.csv')
-extract_one('../../../../' + 'ckanext-odm_laws/ckanext/odm_laws/odm_laws_schema.json', 'odm_laws_schema.csv')
-extract_one('../../../../' + 'ckanext-odm_library/ckanext/odm_library/odm_library_schema.json', 'odm_library_schema.csv')
-extract_one('../../../../' + 'ckanext-odm_agreement/ckanext/odm_agreement/odm_agreement_schema.json', 'odm_agreement_schema.csv')
-extract_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_profiles_schema.json', 'odm_profile.csv')
-extract_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_map_schema.json', 'odm_map.csv')
+def import_one(dest_json, src_csv):
+    with open(src_csv, 'r') as f:
+        f.readline()
+        reader = csv.DictReader(f, fields)
+        translations = { r['Context']: [r[l] for l in languages] for r in reader if r['Context'] }
+
+    with open('translations.json', 'w') as f:
+        json.dump(translations, f, indent=4)
+
+    with open(dest_json, 'r') as f:
+        schema = json.load(f)
+
+    for field in schema['dataset_fields']:
+        for item in ('label', 'form_placeholder', 'help_text'):
+            try:
+                del(field['form_languages'])
+            except: pass
+            context = '.'.join((field['field_name'], item))
+            print "Context: %s" % context
+            if context in translations:
+                existing = field.get(item,{})
+                if type(existing) != type({}):
+                    existing = {'en':existing}
+                existing.update({l:v.decode('utf-8') for l,v in zip(languages, translations[context]) if v})
+                field[item] = existing
+
+    with open (dest_json, 'w') as f:
+        json.dump(schema,f, indent=4)
+
+def extract_all():
+    extract_one('../odm_dataset_schema.json', 'odm_dataset_schema.csv')
+    extract_one('../../../../' + 'ckanext-odm_laws/ckanext/odm_laws/odm_laws_schema.json', 'odm_laws_schema.csv')
+    extract_one('../../../../' + 'ckanext-odm_library/ckanext/odm_library/odm_library_schema.json', 'odm_library_schema.csv')
+    extract_one('../../../../' + 'ckanext-odm_agreement/ckanext/odm_agreement/odm_agreement_schema.json', 'odm_agreement_schema.csv')
+    extract_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_profiles_schema.json', 'odm_profile.csv')
+    extract_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_map_schema.json', 'odm_map.csv')
+
+def import_all():
+    import_one('../odm_dataset_schema_not_required.json', 'odm_dataset_schema.csv')
+    import_one('../odm_dataset_schema_required.json', 'odm_dataset_schema.csv')
+    import_one('../../../../' + 'ckanext-odm_laws/ckanext/odm_laws/odm_laws_schema.json', 'odm_laws_schema.csv')
+    import_one('../../../../' + 'ckanext-odm_library/ckanext/odm_library/odm_library_schema.json', 'odm_library_schema.csv')
+    import_one('../../../../' + 'ckanext-odm_agreement/ckanext/odm_agreement/odm_agreement_schema.json', 'odm_agreement_schema.csv')
+    import_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_profiles_schema.json', 'odm_profile.csv')
+    import_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_map_schema.json', 'odm_map.csv')
+
+import_all()
