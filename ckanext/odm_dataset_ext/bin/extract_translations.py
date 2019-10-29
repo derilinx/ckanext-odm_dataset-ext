6babf140ed05752d7a28d3fee81c1cda1db60a3f
Eric Soroos <eric@derilinx.com>
2019-10-29 13:10:16 +0000

    resource field schema

--- ckanext/odm_dataset_ext/bin/extract_translations.py
+++ ckanext/odm_dataset_ext/bin/extract_translations.py
@@ -6,14 +6,14 @@ fields = ["field_name", "label", 'en', 'Context']
 fields.extend(languages[1:])
 
 
-def extract_one(source, dest):
+def extract_one(source, dest, field_type='dataset_fields'):
 
     with open (source, 'r') as f:
         schema = json.load(f)
 
     translations = []
 
-    for field in schema['dataset_fields']:
+    for field in schema[field_type]:
         if field.get('preset',None) == 'text_hidden': continue
 
 
@@ -40,7 +40,7 @@ def extract_one(source, dest):
         for item in translations:
             writer.writerow(item)
 
-def import_one(dest_json, src_csv):
+def import_one(dest_json, src_csv, field_type='dataset_fields'):
     with open(src_csv, 'r') as f:
         f.readline()
         reader = csv.DictReader(f, fields)
@@ -52,7 +52,7 @@ def import_one(dest_json, src_csv):
     with open(dest_json, 'r') as f:
         schema = json.load(f)
 
-    for field in schema['dataset_fields']:
+    for field in schema[field_type]:
         for item in ('label', 'form_placeholder', 'help_text'):
             try:
                 del(field['form_languages'])
@@ -69,7 +69,7 @@ def import_one(dest_json, src_csv):
     with open (dest_json, 'w') as f:
         json.dump(schema,f, indent=4)
 
-def extract_all():
+def extract_all_dataset():
     extract_one('../odm_dataset_schema.json', 'odm_dataset_schema.csv')
     extract_one('../../../../' + 'ckanext-odm_laws/ckanext/odm_laws/odm_laws_schema.json', 'odm_laws_schema.csv')
     extract_one('../../../../' + 'ckanext-odm_library/ckanext/odm_library/odm_library_schema.json', 'odm_library_schema.csv')
@@ -77,7 +77,13 @@ def extract_all():
     extract_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_profiles_schema.json', 'odm_profile.csv')
     extract_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_map_schema.json', 'odm_map.csv')
 
-def import_all():
+def extract_all_resource():
+    extract_one('../odm_dataset_schema.json', 'odm_dataset_resource_schema.csv', 'resource_fields')
+    extract_one('../../../../' + 'ckanext-odm_laws/ckanext/odm_laws/odm_laws_schema.json', 'odm_laws_resource_schema.csv', 'resource_fields')
+    extract_one('../../../../' + 'ckanext-odm_library/ckanext/odm_library/odm_library_schema.json', 'odm_library_resource_schema.csv', 'resource_fields')
+    extract_one('../../../../' + 'ckanext-odm_agreement/ckanext/odm_agreement/odm_agreement_schema.json', 'odm_agreement_resource_schema.csv', 'resource_fields')
+
+def import_all_dataset():
     import_one('../odm_dataset_schema_not_required.json', 'odm_dataset_schema.csv')
     import_one('../odm_dataset_schema_required.json', 'odm_dataset_schema.csv')
     import_one('../../../../' + 'ckanext-odm_laws/ckanext/odm_laws/odm_laws_schema.json', 'odm_laws_schema.csv')
@@ -86,4 +92,13 @@ def import_all():
     import_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_profiles_schema.json', 'odm_profile.csv')
     import_one('../../../../' + 'ckanext-odm_profiles/ckanext/odm_profiles/odm_map_schema.json', 'odm_map.csv')
 
-import_all()
+def import_all_resource():
+    import_one('../odm_dataset_schema_not_required.json', 'odm_dataset_resource_schema.csv', 'resource_fields')
+    import_one('../odm_dataset_schema_required.json', 'odm_dataset_resource_schema.csv', 'resource_fields')
+    import_one('../../../../' + 'ckanext-odm_laws/ckanext/odm_laws/odm_laws_schema.json', 'odm_laws_resource_schema.csv', 'resource_fields')
+    import_one('../../../../' + 'ckanext-odm_library/ckanext/odm_library/odm_library_schema.json', 'odm_library_resource_schema.csv', 'resource_fields')
+    import_one('../../../../' + 'ckanext-odm_agreement/ckanext/odm_agreement/odm_agreement_schema.json', 'odm_agreement_resource_schema.csv', 'resource_fields')
+
+#extract_all_dataset()
+#extract_all_resource()
+import_all_dataset()
